3723e4b4218a5998ff63e3683e8c5c5a0ebc8b94
Aurélien BLANCHET <BLANCHET.Aurelien@BTSSIO.local>
2023-03-24 14:25:53 +0100

    Initial commit

---
+++ vue/vueAuthentification.php
@@ -0,0 +1,24 @@
+<h1>Connexion</h1>
+<form action="./?action=connexion" method="POST">
+
+    <div class="row g-3">
+        <div class="col-2">
+            <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+        </div>
+        <div class="col-2">
+            <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+        </div>
+    </div>
+        <input type="submit" />
+
+
+
+</form>
+<br />
+<a href="./?action=inscription">Inscription</a>
+
+<hr>
+Utilisateur de test : <br />
+login : test@bts.sio<br />
+mot de passe : sio
+
